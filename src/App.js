f6043f6cb98cecc835efc6a181a20997d94746bc
Patrick Morton <patrickjamesmorton@gmail.com>
2024-03-06 11:40:27 +0000

    made it my own

--- src/App.js
+++ src/App.js
@@ -1,22 +1,14 @@
-import logo from './logo.svg';
+import profile from './profile.jpeg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={profile} className="App-profile" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Hi 👋 I'm Paddy, nice to meet you!
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
