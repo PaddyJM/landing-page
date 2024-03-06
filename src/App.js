e5325ef569de7044b47d2b39529e6d1a559c2b7a
Patrick Morton <patrickjamesmorton@gmail.com>
2024-03-06 12:20:36 +0000

    added framer and improved animations on profile logo

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import profile from './profile.jpeg';
 import './App.css';
+import { motion } from 'framer-motion';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={profile} className="App-profile" alt="logo" />
+        <motion.div initial={{ y: "-200%" }} animate={{ y: 0 }} exit={{ y: -100 }} transition={{ duration: 0.7, delay: 0.1 }}>
+          <motion.button className="App-profile-motion" whileHover={{ scale: 1.1 }} whileTap={{ scale: 0.9 }}>
+            <img src={profile} className="App-profile" alt="logo" />
+          </motion.button>
+        </motion.div>
         <p>
           Hi 👋 I'm Paddy, nice to meet you!
         </p>
