9088a4956bba95853122c5361df0bf571f72bc65
Patrick Morton <patrickjamesmorton@gmail.com>
2024-03-06 12:27:47 +0000

    install prettier and eslint to make it pretty

--- src/App.js
+++ src/App.js
@@ -6,14 +6,19 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <motion.div initial={{ y: "-200%" }} animate={{ y: 0 }} exit={{ y: -100 }} transition={{ duration: 0.7, delay: 0.1 }}>
-          <motion.button className="App-profile-motion" whileHover={{ scale: 1.1 }} whileTap={{ scale: 0.9 }}>
+        <motion.div
+          initial={{ y: '-200%' }}
+          animate={{ y: 0 }}
+          exit={{ y: -100 }}
+          transition={{ duration: 0.7, delay: 0.1 }}>
+          <motion.button
+            className="App-profile-motion"
+            whileHover={{ scale: 1.1 }}
+            whileTap={{ scale: 0.9 }}>
             <img src={profile} className="App-profile" alt="logo" />
           </motion.button>
         </motion.div>
-        <p>
-          Hi 👋 I'm Paddy, nice to meet you!
-        </p>
+        <p>Hi 👋 I'm Paddy, nice to meet you!</p>
       </header>
     </div>
   );
