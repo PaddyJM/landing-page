d042ef55c356ffc15355bf97421833504e8470cb
Patrick Morton <patrickjamesmorton@gmail.com>
2024-03-07 22:39:39 +0000

    upgraded the thing to typescript andsorted out the linting

--- infrastructure/src/infrastructure-stack.ts
+++ infrastructure/src/infrastructure-stack.ts
@@ -1,22 +1,22 @@
-import * as cdk from 'aws-cdk-lib';
-import { Construct } from 'constructs';
+import * as cdk from "aws-cdk-lib";
+import { Construct } from "constructs";
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 export class InfrastructureStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    const bucket = new cdk.aws_s3.Bucket(this, 'LandingPageBucket', {
+    const bucket = new cdk.aws_s3.Bucket(this, "LandingPageBucket", {
       publicReadAccess: true,
-      websiteIndexDocument: 'index.html',
+      websiteIndexDocument: "index.html",
       blockPublicAccess: cdk.aws_s3.BlockPublicAccess.BLOCK_ACLS,
       accessControl: cdk.aws_s3.BucketAccessControl.BUCKET_OWNER_FULL_CONTROL,
-      bucketName: 'patrickmorton.co.uk'
+      bucketName: "patrickmorton.co.uk",
     });
 
-    new cdk.aws_s3_deployment.BucketDeployment(this, 'LandingPageWebsite', {
-      sources: [cdk.aws_s3_deployment.Source.asset('../build')],
-      destinationBucket: bucket
+    new cdk.aws_s3_deployment.BucketDeployment(this, "LandingPageWebsite", {
+      sources: [cdk.aws_s3_deployment.Source.asset("../build")],
+      destinationBucket: bucket,
     });
   }
 }
--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,32 @@
-import profile from './profile.jpeg';
-import './App.css';
-import { motion } from 'framer-motion';
+import profile from "./profile.jpeg";
+import "./App.css";
+import { motion } from "framer-motion";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <motion.div
-          initial={{ y: '-200%' }}
+          initial={{ y: "-200%" }}
           animate={{ y: 0 }}
           exit={{ y: -100 }}
-          transition={{ duration: 0.7, delay: 0.1 }}>
+          transition={{ duration: 0.7, delay: 0.1 }}
+        >
           <motion.button
             className="App-profile-motion"
             whileHover={{ scale: 1.1 }}
-            whileTap={{ scale: 0.9 }}>
+            whileTap={{ scale: 0.9 }}
+          >
             <a href="https://github.com/PaddyJM">
               <img src={profile} className="App-profile" alt="logo" />
             </a>
           </motion.button>
         </motion.div>
         <motion.div
-          initial={{ y: '+200%' }}
+          initial={{ y: "+200%" }}
           animate={{ y: 0 }}
-          transition={{ duration: 0.7, delay: 0.1 }}>
+          transition={{ duration: 0.7, delay: 0.1 }}
+        >
           <p>Hi 👋 I&apos;m Paddy, nice to meet you!</p>
         </motion.div>
       </header>
